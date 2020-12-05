04d1a734315267d4c96c0ca09a6f22ba99daa134
Paulo Maio <pam@isep.ipp.pt>
2020-12-05 01:33:33 +0000

    Some clean up

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
