56de114ffc9d7339a88d6b042b816abd334375f3
1190429 <1190429@isep.ipp.pt>
2021-11-11 22:19:25 +0000

    DI-45 #close #comment UC3 - metodo para editar relação

--- Domain/Relacoes/RelacaoService.cs
+++ Domain/Relacoes/RelacaoService.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using DDDSample1.Domain.Shared;
 using DDDSample1.Domain.Jogadores;
+using DDDSample1.Domain.SharedValueObjects;
 
 namespace DDDSample1.Domain.Relacoes
 {
@@ -23,16 +24,6 @@ namespace DDDSample1.Domain.Relacoes
 
         //     return listDto;
         // }
-
-        public async Task<RelacaoDto> GetByIdAsync(RelacaoId id)
-        {
-            var relacao = await this._repo.GetByIdAsync(id);
-            
-            if(relacao == null)
-                return null;
-
-            return new RelacaoDto{Id = relacao.Id.AsString(), Jogador1 = relacao.Jogador1, Jogador2 = relacao.Jogador2, Tags = relacao.Tags, ForcaRelacao = relacao.ForcaRelacao, ForcaLigacao = relacao.ForcaLigacao};
-        }
 
         public async Task<List<RelacaoDto>> GetRelacoesDoJogador(Jogador jog)
         {
@@ -101,6 +92,42 @@ namespace DDDSample1.Domain.Relacoes
             await this._unitOfWork.CommitAsync();
 
             return new RelacaoDto { Id = relacao.Id.AsString(), Jogador1 = relacao.Jogador1, Jogador2 = relacao.Jogador2, Tags = relacao.Tags, ForcaRelacao = relacao.ForcaRelacao, ForcaLigacao = relacao.ForcaLigacao };
+        }
+
+        public async Task<RelacaoDto> PatchRelacaoTagsForca(RelacaoDto dto) {
+            var relacao = await this._repo.GetByIdAsync(new RelacaoId(dto.Id)); 
+
+            if (relacao == null)
+                return null;  
+
+            // change all field
+            relacao.ChangeTags(converteParaListaString(dto.Tags));
+
+            relacao.ChangeForcaLigacao(dto.ForcaLigacao.Valor);
+            
+            await this._unitOfWork.CommitAsync();
+
+            return new RelacaoDto { Id = relacao.Id.AsString(), Jogador1 = relacao.Jogador1, Jogador2 = relacao.Jogador2, Tags = relacao.Tags, ForcaRelacao = relacao.ForcaRelacao, ForcaLigacao = relacao.ForcaLigacao };
+        }
+
+        public static List<string> converteParaListaString(List<Tag> lista){
+            List<string> ls = new List<string>();
+            foreach (Tag tag in lista){
+                ls.Add(tag.Descricao);
+            }
+            return ls;
+        }
+
+        public async Task<List<RelacaoDto>> ToListAsync()
+        {
+            var relacao = await this._repo.GetAllAsync();
+            
+            if(relacao == null)
+                return null;
+
+            List<RelacaoDto> lista = relacao.ConvertAll<RelacaoDto>( relacao => new RelacaoDto{ Id = relacao.Id.AsString(), Jogador1 = relacao.Jogador1, Jogador2 = relacao.Jogador2, Tags = relacao.Tags, ForcaRelacao = relacao.ForcaRelacao, ForcaLigacao = relacao.ForcaLigacao});
+
+            return lista;
         }
     }
 }
