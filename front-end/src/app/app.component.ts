925a5757f32307fc792ec0c1af9f9131fdc7122c
1190429 <1190429@isep.ipp.pt>
2021-11-23 16:38:46 +0000

    DI-89 #close # Esqueleto da SPA

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Social Network';
+  p1 = 'O Jogo simula uma rede social e o jogador tem por objetivo expandir a sua rede social, com o objetivo último de ter a maior e mais forte rede social possível. O jogo desenrola-se numa série de missões em que o jogador terá que avançar e aumentar a sua rede para subir no leader board. Um utilizador pode iniciar um jogo/missão em qualquer momento escolhendo o nível de dificuldade pretendido.';
+
 }
