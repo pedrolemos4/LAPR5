de46d0a8b1b96a8c80819cb44a25255311496e4d
Tiago_1191098 <1191098@isep.ipp.pt>
2021-12-02 23:45:11 +0000

    DI-82 #close #comment Comparação de Tags

---
+++ Planeamento/checktags.pl
@@ -0,0 +1,30 @@
+plan_x_tags(NTags,Res):-
+findall(Tag,(no(_,_,LTags), member(Tag,LTags)),LTodosTagsRep),
+removeRepetidos(LTodosTagsRep,ListaTags),
+todas_combinacoes(NTags,ListaTags,ListaCombinacoes),
+findall(X,no(X,_,_),Lista),
+ListaAux = [],
+comparar(ListaCombinacoes,Lista,ListaFinal,ListaAux,Res).
+
+removeRepetidos([],[]):-!.
+removeRepetidos([X|LTodosTagsRep],ListaTags):- member(X,LTodosTagsRep),!,removeRepetidos(LTodosTagsRep,ListaTags).
+removeRepetidos([X|LTodosTagsRep],[X|ListaTags]):- removeRepetidos(LTodosTagsRep,ListaTags).
+
+todas_combinacoes(X,LTags,LcombXTags):-findall(L,combinacao(X,LTags,L),LcombXTags).
+combinacao(0,_,[]):-!.
+combinacao(X,[Tag|L],[Tag|T]):-X1 is X-1, combinacao(X1,L,T).
+combinacao(X,[_|L],T):- combinacao(X,L,T).
+
+comparar([],_,_,ListaAux,ListaAux):-!.
+comparar([Comb|ListaCombinacoes],Lista,ListaFinal,ListaAux,Res):-comparar1(Comb,Lista,ListaFinal,_),comparar(ListaCombinacoes,Lista,_,[Comb,ListaFinal|ListaAux],Res),!.
+comparar1(_,[],[],_):-!.
+comparar1(Comb,[Id|Lista],ListaFinal,_):- no(Id,_,ListaTags),intersection(Comb,ListaTags,LInter),length(LInter,TamFinal),length(Comb,TamComb),(TamFinal \== TamComb),!,comparar1(Comb,Lista,ListaFinal,_).
+comparar1(Comb,[Id|Lista],[Id|ListaFinal],_):- comparar1(Comb,Lista,ListaFinal,_).
+
+
+
+
+
+
+
+
