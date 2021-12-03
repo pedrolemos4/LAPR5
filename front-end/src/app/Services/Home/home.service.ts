011ed3cedc8afb3c79c14815119752c954cf1b3f
1190974 <1190974@isep.ipp.pt>
2021-12-03 00:31:48 +0000

    DI-98 #close #comment Implementação visual do caso de uso

--- front-end/src/app/Services/Home/home.service.ts
+++ front-end/src/app/Services/Home/home.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Jogador } from 'src/app/Models/Jogador';
+import { Relacao } from 'src/app/Models/Relacao';
+import { Perfil } from 'src/app/Models/Perfil';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,11 +11,25 @@ import { environment } from 'src/environments/environment';
 })
 export class HomeService {
 
-  private readonly urlPer = environment.apiUrl + 'Perfis';
+  private readonly urlPer = environment.apiUrl + 'Perfis/';
+  private readonly jogadorUrl = environment.apiUrl + 'Jogadores/';
+  private readonly relacoesUrl = environment.apiUrl + 'Relacoes/';
 
   constructor(private http: HttpClient) { }
 
-  getPerfilByEmail(email:string): Observable<any> {
-    return this.http.get(this.urlPer + '/GetPerfilByEmail/' + email);
+  getPerfilByEmail(email: string): Observable<Perfil> {
+    return this.http.get<Perfil>(this.urlPer + 'GetPerfilByEmail/' + email);
+  }
+
+  getJogadorAtual(perfil: any): Observable<Jogador> {
+    return this.http.get<Jogador>(this.jogadorUrl + 'GetJogadorByPerfil/' + perfil);
+  }
+
+  getPerfil(id: any): Observable<Perfil> {
+    return this.http.get<Perfil>(this.jogadorUrl + 'GetPerfilJogador/' + id);
+  }
+
+  getRelacoesJogador(id: any): Observable<Relacao []>{
+    return this.http.get<Relacao []>(this.relacoesUrl + 'GetRelacoesDoJogador/' + id);
   }
 }
