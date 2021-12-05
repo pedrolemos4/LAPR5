ddfd36c0703fb03886205870013475e24b7cf564
1190429 <1190429@isep.ipp.pt>
2021-12-05 17:44:51 +0000

    DI-79 #close #comment Base de Conhecimento com ligação ao master data

--- Planeamento/server.pl
+++ Planeamento/server.pl
@@ -1,170 +1,126 @@
-% Bibliotecas HTTP
+% Bibliotecas
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_parameters)).
 :- use_module(library(http/http_open)).
-:- use_module(library(http/http_cors)).
-:- use_module(library(date)).
-:- use_module(library(random)).
-
-% Bibliotecas JSON
-:- use_module(library(http/json_convert)).
-:- use_module(library(http/http_json)).
 :- use_module(library(http/json)).
 
+% Relacao entre pedidos HTTP e predicados que os processam
+:- http_handler('/lapr5', responde_ola, []).
+:- http_handler('/register_user', register_user, []).
+:- http_handler('/send_file_post', send_file_post, []).
+%:- http_handler('/api/Jogadores', getJogadores, []).
+%:- http_handler('/api/Perfis', getPerfis, []).
 
-%% Nossos modules
-
-:-ensure_loaded("./AlgoritmoCaminhoSeguro.pl").
-:-ensure_loaded("./CalcularTamanhoRede.pl").
-:-ensure_loaded("./checktags.pl").
-:-ensure_loaded("./SugerirConexoesTagsNivel.pl").
-
-
-
-% Objectos Json a receber no prolog
-:- json_object caminho_seguro_user_json(caminho:list(string),forcaResultante:integer).
-
-
-
-
-%% Base Conhecimento principal
-
+:- dynamic novo_jogador/2.
+:- dynamic novo_perfil/3.
+:- dynamic nova_relacao/3.
 :- dynamic no/3.
 :- dynamic ligacao/4.
 
+% Criacao de servidor HTTP no porto 'Port'
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
 
+% Tratamento de 'http://localhost:5000/lapr5'
+responde_ola(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Olá LAPR5!~n').
 
-% Gerir servidor
-startServer(Port) :-
-    http_server(http_dispatch, [port(Port)]),
-    asserta(port(Port)).
+% MÉTODO GET: Tratamento de 'http://localhost:5000/register_user?name='José'&sex=male&birth_year=1975'
+% ou http_client:http_get('http://localhost:5000/register_user?name=\'José\'&sex=male&birth_year=1975',X,[]).
 
-stopServer:-
-    retract(port(Port)),
-    http_stop_server(Port,_).
-
-
-%Cors
-:- set_setting(http:cors, [*]).
-
-
-
-%Porta default para o server
-default_server_port(4200).
-
-%url da API
-
-obter_players_url("https://localhost:5001/api/Jogadores").
+% MÉTODO POST
+% http_client:http_post('http://localhost:5000/register_user', form_data([name='José', sex=male, birth_year=1975]), Reply, []).
+obter_users_url("https://localhost:5001/api/Jogadores").
 obter_perfis_url("https://localhost:5001/api/Perfis").
-obter_ligacoes_url("https://localhost:5001/api/Relacoes").
-
-addClients():-
-    obterJogadores(DataJogadores),
-	obterPerfis(DataPerfis),
-    parse_users(DataJogadores,DataPerfis,DataPerfis).
-
-%% Obter users Registados (Data nao convertida)
-
-obterJogadores(DataJogadores):-
-    obter_players_url(URL),
-    setup_call_cleanup(
-        http_open(URL, In, [ cert_verify_hook(cert_accept_any)]),
-        json_read_dict(In, DataJogadores),
-        close(In)
-).
-
-obterPerfis(DataPerfis):-
-	obter_perfis_url(URL),
-    setup_call_cleanup(
-        http_open(URL, In, [ cert_verify_hook(cert_accept_any)]),
-        json_read_dict(In, DataPerfis),
-        close(In)
-).
-
-%% Convertes Json de todos Users em Lista
-
-parse_users([],_,_).
-parse_users([H|ListJogadores], [P|_],ListaAux):- H.get(Perfil) == P.get(Id),
-    asserta(no(H.get(id),P.get(email),P.get(tags))),
-    parse_users(ListJogadores,ListaAux,ListaAux).
-parse_users([H|ListJogadores], [_|ListaPerfis],ListaAux):- parse_users([H|ListJogadores],ListaPerfis, ListaAux).
+obter_relacoes_url("https://localhost:5001/api/Relacoes").
 
 
-%% Colocar as ligacoes da api dinamicamente no prolog
-adicionarLigacoes():-
-    obterLigacoes(Data),
-    parse_ligacoes(Data).
+adicionaJogadores():-
+    carregaJogadores(Data),
+    parse_jogadores(Data).
 
-
-obterLigacoes(Data):-
-    obter_ligacoes_url(URL),
+carregaJogadores(Data) :-
+    obter_users_url(URL),
     setup_call_cleanup(
         http_open(URL, In, [ cert_verify_hook(cert_accept_any)]),
         json_read_dict(In, Data),
         close(In)
 ).
 
-parse_ligacoes([]).
-parse_ligacoes([H|List]):-
-    asserta(ligacao(H.get(oUser),H.get(dUser),H.get(connectionStrengthOUser),H.get(connectionStrengthDUser))),
-    parse_ligacoes(List).
+parse_jogadores([]).
+parse_jogadores([H|List]):-
+    asserta(novo_jogador(H.get(id),H.get(perfilId))),
+    parse_jogadores(List).
 
 
-% Remover dados do sistema
+adicionaPerfis():-
+    carregaPerfis(Data),
+    parse_perfis(Data).
 
+carregaPerfis(Data) :-
+    obter_perfis_url(URL),
+    setup_call_cleanup(
+        http_open(URL, In, [ cert_verify_hook(cert_accept_any)]),
+        json_read_dict(In, Data),
+        close(In)
+).
+
+parse_perfis([]).
+parse_perfis([H|List]):-
+    asserta(novo_perfil(H.get(id),H.get(email),H.get(tags))),
+    parse_perfis(List).
+
+
+adicionaRelacoes():-
+    carregaRelacoes(Data),
+    parse_relacoes(Data).
+
+carregaRelacoes(Data) :-
+    obter_relacoes_url(URL),
+    setup_call_cleanup(
+        http_open(URL, In, [ cert_verify_hook(cert_accept_any)]),
+        json_read_dict(In, Data),
+        close(In)).
+
+parse_relacoes([]).
+parse_relacoes([H|List]):-
+    asserta(nova_relacao(H.get(jogador1),H.get(jogador2),H.get(forcaLigacao))),
+    parse_relacoes(List).
+
+
+carregaLigacoes([]):- !.
+carregaLigacoes([Jogador1|Lista]):-
+    nova_relacao(Jogador1,Jogador2,ForcaLigacao1),nova_relacao(Jogador2,Jogador1,ForcaLigacao2),
+    asserta(ligacao(Jogador1,Jogador2,ForcaLigacao1,ForcaLigacao2)),carregaLigacoes(Lista).
+carregaLigacao([_|Lista]):-carregaLigacoes(Lista).
+
+adicionaLigacoes():- findall(Jogador1,nova_relacao(Jogador1,_,_),Lista),
+                        carregaLigacao(Lista).
+
+carregaDados():-
+    adicionaPerfis(),
+    adicionaJogadores(),
+    adicionaRelacoes(),
+    adicionaLigacoes().
+
+
+% Remover tudo da Base de Conhecimento
 removerBaseConhecimento():-
+        retractall(novo_jogador(_,_)),
+        retractall(novo_perfil(_,_,_)),
+        retractall(nova_relacao(_,_,_)),
         retractall(no(_,_,_)),
         retractall(ligacao(_,_,_,_)).
 
 
-% Handlers
-:- http_handler('/api/arroz', obter_caminho_seguro_unidirecional_handler, []).
-:- http_handler('/api/caminhoSeguroBidirecional', obter_caminho_seguro_bidirecional_handler, []).
 
-obter_caminho_seguro_unidirecional_handler(_):-
+ %MÉTODO POST enviando um ficheiro de texto
+ %http_client:http_post('http://localhost:5000/send_file_post', form_data([file=file('./teste.txt')]), Reply, []).
+
+%comentar ou descomentar send_file_post?
+send_file_post(Request) :-
+http_parameters(Request,[ file(X,[])]),
     format('Content-type: text/plain~n~n'),
-		format('Olá LAPR5!~n'),
-	        prolog_to_json('Olá LAPR5!~n',JSONObject),
-		reply_json(JSONObject, [json_object(dict)]).
-    %cors_enable,
-    %removerBaseConhecimento(),
-    %carregar_dados_sistema(),
-    %http_parameters(Request,
-    %    [origId(Orig,[string]),
-    %     destId(Dest,[string]),
-    %     minLigacao(MinLigacao,[integer,between(0,100)])] ),
-%
-%
-%    caminho_seguro_unidirecional(Orig,Dest,MinLigacao,LCaminho,Forca),
-%    Reply=caminho_seguro_user_json(LCaminho,Forca),
-%    prolog_to_json(Reply, JSONObject),
-%    reply_json(JSONObject,[json_object]).
-
-obter_caminho_seguro_bidirecional_handler(Request):-
-    cors_enable,
-    removerBaseConhecimento(),
-    carregar_dados_sistema(),
-    http_parameters(Request,
-        [origId(Orig,[string]),
-         destId(Dest,[string]),
-         minLigacao(MinLigacao,[integer,between(0,100)])] ),
-
-    caminho_seguro_bidirecional(Orig,Dest,MinLigacao,LCaminho,Forca),
-    Reply=caminho_seguro_user_json(LCaminho,Forca),
-    prolog_to_json(Reply, JSONObject),
-    reply_json(JSONObject,[json_object]).
-
-
-
-%MÃ©todos que carregam os dados para sistema
-carregar_dados_sistema():-
-    addClients(),
-    adicionarLigacoes().
-
-inicializar_server:-
-    default_server_port(Port),
-    startServer(Port),!,
-    carregar_dados_sistema(),!.
-
-:- inicializar_server.
+    format('Received: ~w~n',[X]).
