a699abb5b5235d495054c1c8488a7243fe4ccf83
1190429 <1190429@isep.ipp.pt>
2021-12-05 22:44:27 +0000

    DI-105 #close #comment Logout

--- front-end/src/app/Components/header/header.component.ts
+++ front-end/src/app/Components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  Logout(){
+    localStorage.removeItem('currentUser');
+    console.log(localStorage.getItem('currentUser'));
+    this.toastr.success('Logout realizado com sucesso!');
+    this.router.navigateByUrl('/homePrincipal');
+    
+  }
 }
