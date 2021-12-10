79c33014ad72dd4591cb5a41c69ebb7373ee4b2e
1190974 <1190974@isep.ipp.pt>
2021-12-10 23:16:37 +0000

    DI-160 #close #comment Demonstração do caminho mais forte

--- Planeamento/CaminhoMaisForte.pl
+++ Planeamento/CaminhoMaisForte.pl
@@ -11,12 +11,8 @@ no(NX,X,_),\+ member(X,LA),dfs2(X,Dest,[X|LA],Cam,S,Sfinal1).
 
 
 plan_forte(Orig,Dest,LCaminho):-
-get_time(Ti),
 (caminho_mais_forte(Orig,Dest);true),
-retract(melhor_sol_forte(LCaminho,_)),
-get_time(Tf),
-T is Tf-Ti,
-write('Tempo de geracao da solucao:'),write(T),nl.
+retract(melhor_sol_forte(LCaminho,_)).
 
 caminho_mais_forte(Orig,Dest):-
 asserta(melhor_sol_forte(_,0)),
@@ -29,22 +25,3 @@ melhor_sol_forte(_,N),
 SomaFinal > N,
 retract(melhor_sol_forte(_,_)),
 asserta(melhor_sol_forte(LCaminho,SomaFinal)).
-
-
-%Com findAll
-
-
-%mais seguro com findall
-plan_forte_com_findAll(Orig,Dest,LCaminho):-
-get_time(Ti),
- findall(Caminho,dfs_forte_com_findAll(Orig,Dest,Caminho),LCaminho),
- length(LCaminho,NLCam),get_time(Tf),
-write(' Foram encontradas '), write(NLCam),write(' solucoes em '),
-T is Tf-Ti,write(T),write(' segundos'),nl,
-write(' Lista de Caminhos possiveis: '),nl,nl.
-
-
-dfs_forte_com_findAll(Orig,Dest,Cam):-dfs2_forte_com_findAll(Orig,Dest,[Orig],Cam).
-dfs2_forte_com_findAll(Dest,Dest,LA,Cam):-!,reverse(LA,Cam).
-dfs2_forte_com_findAll(Act,Dest,LA,Cam):-no(NAct,Act,_),(ligacao(NAct,NX,_,_);ligacao(NX,NAct,_,_)),
-no(NX,X,_),\+ member(X,LA),dfs2_forte_com_findAll(X,Dest,[X|LA],Cam).
--- front-end/src/app/Components/cam-forte/cam-forte.component.ts
+++ front-end/src/app/Components/cam-forte/cam-forte.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CamForteService } from 'src/app/Services/CamForte/cam-forte.service';
+import { Perfil } from 'src/app/Models/Perfil';
+import { Jogador } from 'src/app/Models/Jogador';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cam-forte',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamForteComponent implements OnInit {
 
-  constructor() { }
+  emailCurrentUser: string | undefined = '';
+  perfilCurrentUser!: Perfil;
+  idPerfilCurrentUser: string = '';
+  currentUser!: Jogador;
+  idCurrentUser: any = '';
+  amigos: Jogador[] = [];
+  emailAmigos: string[] = [];
+  amigosIdList: string[] = [];
+  amigosPerfilList: Perfil[] = [];
+  selectedAmigo: string = '';
+  form: FormGroup;
+  caminho: string;
+
+
+
+  constructor(private formBuilder: FormBuilder, private camForteService: CamForteService, private toastr: ToastrService) {
+    this.form = this.formBuilder.group({});
+  }
 
   ngOnInit(): void {
+    const currentUser = localStorage.getItem('currentUser');
+    this.emailCurrentUser = currentUser?.replace(/\"/g, "");
+    this.camForteService.getPerfilAtual(this.emailCurrentUser).subscribe(Perfil => {
+      this.perfilCurrentUser = Perfil;
+      this.idPerfilCurrentUser = Perfil.id;
+      this.camForteService.getJogador(this.idPerfilCurrentUser).subscribe(Jogador => {
+        this.currentUser = Jogador;
+        this.idCurrentUser = this.currentUser.id;
+
+        this.camForteService.getAmigos(this.idCurrentUser).subscribe(Amigos => {
+          this.amigos = Amigos;
+          this.amigos.forEach((element: any) => {
+            this.amigosIdList.push(element.id);
+          });
+          this.amigosIdList.forEach((element: any) => {
+            this.camForteService.getPerfilJogador(element).subscribe(Perfil => {
+              this.amigosPerfilList.push(Perfil.id);
+              this.emailAmigos.push(Perfil.email);
+            });
+          });
+        });
+      });
+    });
+  };
+
+  selectAmigo(event: any) {
+    this.selectedAmigo = event.target.value;
+  }
+
+  onSubmit() {
+    this.camForteService.getPerfilAtual(this.selectedAmigo).subscribe(Perfil => {
+      this.camForteService.getCaminhoForte(this.emailCurrentUser, Perfil.email).subscribe(Cam => {
+        var aux = Object.values(Cam);
+        var valores = aux[0];
+        this.caminho = valores;
+        this.toastr.success("Caminho mais forte calculado");
+      });
+    });
   }
 
 }
