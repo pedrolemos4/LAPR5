56315578dd0ddb64b26e2d67096939ce7f67d569
1190769 <1190769@isep.ipp.pt>
2021-12-11 22:56:16 +0000

    DI-160 #close #comment Correção de ligações ao prolog

--- front-end/src/app/Components/tam-rede/tam-rede.component.ts
+++ front-end/src/app/Components/tam-rede/tam-rede.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Perfil } from 'src/app/Models/Perfil';
+import { Jogador } from 'src/app/Models/Jogador';
+import { TamRedeService } from 'src/app/Services/TamRede/tam-rede.service';
 import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tam-rede',
@@ -11,17 +13,39 @@ import { Router } from '@angular/router';
 export class TamRedeComponent implements OnInit {
   tamRedeForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  emailCurrentUser: string | undefined = '';
+  perfilCurrentUser!: Perfil;
+  idPerfilCurrentUser: string = '';
+  currentUser!: Jogador;
+  idCurrentUser: any = '';
+
+  constructor(private formBuilder: FormBuilder, private tamRedeService: TamRedeService, private toastr: ToastrService) { 
     this.tamRedeForm = this.formBuilder.group({
       nivel: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {
-  }
+    const currentUser = localStorage.getItem('currentUser');
+    this.emailCurrentUser = currentUser?.replace(/\"/g, "");
+    this.tamRedeService.getPerfilAtual(this.emailCurrentUser).subscribe(Perfil => {
+      this.perfilCurrentUser = Perfil;
+      this.idPerfilCurrentUser = Perfil.id;
+      this.tamRedeService.getJogador(this.idPerfilCurrentUser).subscribe(Jogador => {
+        this.currentUser = Jogador;
+        this.idCurrentUser = this.currentUser.id;
+      });
+    });
+  };
 
   onSubmit(){
-    //ligacao com prolog
+    var nivel = document.getElementById('nivel') as HTMLInputElement;
+    console.log(nivel.value);
+    this.tamRedeService.getTamRede(nivel.value, this.idCurrentUser).subscribe(Rede => {
+      var res = Object.values(Rede);
+      var tamanho = res[0];
+      this.toastr.success("Tamanho da Rede: " + tamanho);
+    });
   }
 
 }
--- front-end/src/app/Services/TamRede/tam-rede.service.ts
+++ front-end/src/app/Services/TamRede/tam-rede.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Jogador } from 'src/app/Models/Jogador';
+import { Perfil } from 'src/app/Models/Perfil';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,7 +11,20 @@ import { environment } from 'src/environments/environment';
 export class TamRedeService {
 
   private readonly url = environment.apiUrl;  // URL to web api
+  private readonly urlPlan = 'http://localhost:4300';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
   
+  getPerfilAtual(email: any): Observable<Perfil> {
+    return this.http.get<Perfil>(this.url + 'perfis/GetPerfilByEmail/' + email);
+  }
+
+  getJogador(idPerfil: any): Observable<Jogador> {
+    return this.http.get<Jogador>(this.url + 'jogadores/GetJogadorByPerfil/' + idPerfil);
+  }
+
+  getTamRede(Nivel: string, IdJogador: any): Observable<string> {
+    const urlAux = this.urlPlan + '/api/CalcularTamanhoRede?idJog=' + IdJogador + '&nivel=' + Nivel;
+    return this.http.get<string>(urlAux);
+  }
 }
