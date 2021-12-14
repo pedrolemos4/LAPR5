4ee45589fc8990a89b8f06033912d14c50375bb4
Tiago_1191098 <1191098@isep.ipp.pt>
2021-12-14 15:35:10 +0000

    DI-137 #close #comment Continuação de desenvolvimento de SPA

--- front-end/src/app/Components/fazer-post/fazer-post.component.ts
+++ front-end/src/app/Components/fazer-post/fazer-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FazerPostService } from 'src/app/Services/FazerPost/fazer-post.service';
 
 @Component({
   selector: 'app-fazer-post',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FazerPostComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private fazerPostService: FazerPostService, private toastr: ToastrService, private router: Router) { 
+    this.postForm = this.formBuilder.group({
+      TextoPost: ['', Validators.required],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+  }
 }
