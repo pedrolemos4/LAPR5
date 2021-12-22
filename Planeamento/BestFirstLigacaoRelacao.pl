c42758ee826d83d00b127e087cef7bddd1e65efc
1190769 <1190769@isep.ipp.pt>
2021-12-22 17:54:32 +0000

    DI-133 #close #comment Correção algoritmo best first

--- Planeamento/BestFirstLigacaoRelacao.pl
+++ Planeamento/BestFirstLigacaoRelacao.pl
@@ -33,5 +33,9 @@ member1(LA,[_|LAA],LAA1):-member1(LA,LAA,LAA1).
 retira_custos([],[]).
 retira_custos([(_,LA)|L],[LA|L1]):-retira_custos(L,L1).
 
-calcula_custo([Act,X],C,Ligacoes):-!,Ligacoes>0,ligacao(Act,X,FL,FR),C is (FL+FR)/2.
-calcula_custo([Act,X|L],S,Ligacoes):-Ligacoes1 is Ligacoes-1,calcula_custo([X|L],S1,Ligacoes1),ligacao(Act,X,FL,FR),S is S1+((FL+FR)/2).
+calcula_custo([Act,X],C,Ligacoes):-!,Ligacoes>0,ligacao(Act,X,FL,FR),FR>=0,FR<201,C is (FL/2)+(25+(FR/8)).
+calcula_custo([Act,X],C,Ligacoes):-!,Ligacoes>0,ligacao(Act,X,FL,FR),FR>(-201),C is (FL/2)+(25-(abs(FR)/8)).
+calcula_custo([Act,X|L],S,Ligacoes):-Ligacoes1 is Ligacoes-1,calcula_custo([X|L],S1,Ligacoes1),ligacao(Act,X,FL,FR),
+	FR>0,FR<201,S is S1+(FL/2)+(25+(FR/8)).
+calcula_custo([Act,X|L],S,Ligacoes):-Ligacoes1 is Ligacoes-1,calcula_custo([X|L],S1,Ligacoes1),ligacao(Act,X,FL,FR),
+	FR>(-201),S is S1+(FL/2)+(25-(abs(FR)/8)).
