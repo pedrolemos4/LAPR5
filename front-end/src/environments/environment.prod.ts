c85fe203293718f7bc48a90310e9a72b834aba4c
Tiago_1191098 <1191098@isep.ipp.pt>
2021-12-26 16:47:00 +0000

    DI-160 #close #comment Correção de links

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-lapr-5.azurewebsites.net/api',
+  apiUrl: 'https://backend-lapr-5.azurewebsites.net/api/',
   prologUrl : 'https://algav-machine.westeurope.cloudapp.azure.com:443'
 };
