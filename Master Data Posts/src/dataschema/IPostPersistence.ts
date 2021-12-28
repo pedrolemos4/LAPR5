24e7d35e857236512bf3853746b03191291fdc4a
1190769 <1190769@isep.ipp.pt>
2021-12-28 17:00:36 +0000

    DI-129 #close #comment Correções

--- Master Data Posts/src/dataschema/IPostPersistence.ts
+++ Master Data Posts/src/dataschema/IPostPersistence.ts
@@ -4,6 +4,7 @@ export interface IPostPersistence{
     domainId: string;
     description: string;
     email: string;
+    listaComentarios: List<string>;
     likes: List<string>;
     dislikes: List<string>;
 }
