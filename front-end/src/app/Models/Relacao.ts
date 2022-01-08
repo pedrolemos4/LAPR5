03cb19ffb38a892ac7b6a6be8961ad895ed60c0f
1190974 <1190974@isep.ipp.pt>
2022-01-08 22:45:39 +0000

    DI-160 #close #comment Correção

--- front-end/src/app/Models/Relacao.ts
+++ front-end/src/app/Models/Relacao.ts
@@ -2,7 +2,7 @@ export interface Relacao{
     id : any;
     jogador1 : any;
     jogador2 : any;
-    tags : string[];
+    tags : string[] | any;
     forcaRelacao : number;
     forcaLigacao : number;
 }
