e7f921080841c82eacb577648e71b8550c3c6922
1190974 <1190974@isep.ipp.pt>
2022-01-10 17:29:17 +0000

    DI-160 #close #comment Correções

--- front-end/src/app/Components/home/home.component.ts
+++ front-end/src/app/Components/home/home.component.ts
@@ -42,7 +42,8 @@ export class HomeComponent implements OnInit {
     this.emailUser = currentUser?.replace(/\"/g, "");
     this.homeService.getPerfilByEmail(this.emailUser).subscribe(Perfil => {
       this.nomes.push(Perfil.nome);
-      console.log(Perfil.tags.length + " ," + this.numeroTags + " 45");
+      console.log(Perfil.nome+" 45");
+      console.log(Perfil.tags.length + " ," + this.numeroTags + " 46");
       if (this.numeroTags <= Perfil.tags.length) {
         this.homeService.getJogadorAtual(Perfil.id).subscribe(Jogador => {
           this.homeService.getRelacoesJogador(Jogador.id).subscribe(result => {
