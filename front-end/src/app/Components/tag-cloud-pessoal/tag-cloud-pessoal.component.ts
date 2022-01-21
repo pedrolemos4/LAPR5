eda33fc9865a9f07cfae5b85df8fb1b54de00a23
Tiago_1191098 <1191098@isep.ipp.pt>
2022-01-21 09:39:01 +0000

    DI-157 #close #comment Correção Tag Cloud Pessoal

--- front-end/src/app/Components/tag-cloud-pessoal/tag-cloud-pessoal.component.ts
+++ front-end/src/app/Components/tag-cloud-pessoal/tag-cloud-pessoal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CloudData, CloudOptions, TagCloudComponent } from 'angular-tag-cloud-module';
+import { ToastrService } from 'ngx-toastr';
+import { element } from 'protractor';
+import { Post } from 'src/app/Models/Post';
 import { TagCloudPessoalService } from 'src/app/Services/TagCloudPessoal/tag-cloud-pessoal.service';
 
 @Component({
@@ -7,24 +11,67 @@ import { TagCloudPessoalService } from 'src/app/Services/TagCloudPessoal/tag-clo
   styleUrls: ['./tag-cloud-pessoal.component.css']
 })
 export class TagCloudPessoalComponent implements OnInit {
+  @ViewChild(TagCloudComponent) tagCloudComponent: TagCloudComponent;
+
+  options: CloudOptions = {
+    width: 0.9,
+    height: 200,
+    overflow: false,
+    zoomOnHover: {
+      scale: 1.2,
+      transitionTime: 0.3,
+      delay: 0.3
+    },
+    realignOnResize: true
+  }
 
   listJogadores: string[] = new Array<string>();
   emailCurrentUser: string | undefined = '';
   listTags: string[] = new Array<string>();
 
-  constructor(private tagsPessoaisService: TagCloudPessoalService) { }
+  data: CloudData[] = [{
+    text: '',
+    weight: 2,
+    color: '#ffaaee',
+    link: null,
+    rotate: null
+  }];
+  dict = {};
+
+  constructor(private tagsPessoaisService: TagCloudPessoalService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     const currentUser = localStorage.getItem('currentUser');
     this.emailCurrentUser = currentUser?.replace(/\"/g, "");
-    this.tagsPessoaisService.getPerfilAtual(this.emailCurrentUser).subscribe(Perfil => {
-      Perfil.tags.forEach(element => {
-        this.listTags.push(element);
-      });
+    this.tagsPessoaisService.getPosts(this.emailCurrentUser).subscribe((res: Post[]) => {
+      if (res.length == 0) {
+        this.toastr.error("Ainda não criou qualquer post", undefined, { positionClass: 'toast-bottom-left' });
+      } else {
+        res.forEach(element => {
+          element.tags.forEach(tag => {
+            if (this.listTags.includes(tag) == false) {
+              this.listTags.push(tag);
+              this.dict[tag] = 0; 
+            }
+            var x = this.dict[tag];
+              this.dict[tag] = x + 1;
+          });
+        });
+      }
+      for (let key in this.dict) {
+        var color: string = this.generateRandomCode();
+        this.putOnCloud(key, this.dict[key], color);
+        
+      }
     });
+    /* this.tagsPessoaisService.getPerfilAtual(this.emailCurrentUser).subscribe(Perfil => {
+       Perfil.tags.forEach(element => {
+         this.listTags.push(element);
+       });
+     });*/
   }
 
-  jogadoresTag(tag: string) {
+  /*jogadoresTag(tag: string) {
     this.tagsPessoaisService.getAllPerfis().subscribe(
       (res: any) => {
         res.forEach(element => {
@@ -34,6 +81,29 @@ export class TagCloudPessoalComponent implements OnInit {
         });
       });
     this.listJogadores = [];
+  }*/
+
+
+  putOnCloud(texto: any, peso: any, color1: any) {
+    var x: CloudData = {
+      text: texto,
+      weight: peso,
+      color: color1,
+      link: null,
+      rotate: null
+    };
+    this.data.push(x);
+    console.log(this.data);
   }
+
+  generateRandomCode() {
+    var myRandomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return myRandomColor;
+  }
+
+  reDraw() {
+    this.tagCloudComponent.reDraw();
+  }
+
 
 }
--- front-end/src/app/Services/TagCloudPessoal/tag-cloud-pessoal.service.ts
+++ front-end/src/app/Services/TagCloudPessoal/tag-cloud-pessoal.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Perfil } from 'src/app/Models/Perfil';
+import { Post } from 'src/app/Models/Post';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,14 +11,19 @@ import { environment } from 'src/environments/environment';
 export class TagCloudPessoalService {
 
   private readonly perfilUrl = environment.apiUrl + 'Perfis/';
-
+  private readonly postUrl = environment.masterDataPostsUrl + 'posts';
   constructor(private http: HttpClient) { }
 
   getPerfilAtual(email: any): Observable<Perfil> {
     return this.http.get<Perfil>(this.perfilUrl + 'GetPerfilByEmail/' + email);
   }
-  
+
   getAllPerfis() {
     return this.http.get<Perfil[]>(this.perfilUrl);
+  }
+
+  getPosts(email: string): Observable<Post[]> {
+    const params = new HttpParams().append('param', email);
+    return this.http.get<Post[]>(this.postUrl + '/getPostsByEmail', { params });
   }
 }
